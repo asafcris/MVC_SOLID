e39c7ba75d51f1acf8daf68422b310717058dda3
asafcris <30936234+asafcris@users.noreply.github.com>
2017-10-11 21:23:52 -0300

    iImplementações
    TDD, GridView

--- Aula_MVC.Web/App_Start/BundleConfig.cs
+++ Aula_MVC.Web/App_Start/BundleConfig.cs
@@ -22,10 +22,12 @@ namespace Aula_MVC.Web
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/custon.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/gridmvc.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/Gridmvc.css",
                       "~/Content/site.css"));
         }
     }
